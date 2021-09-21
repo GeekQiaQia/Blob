12fff70faa59e14d189b5456d21a04b1dda336f4
xiyang.bai <xiyang.bai@zhangmen.com>
2021-09-21 23:06:07 +0800

    update-coding

--- coding/codeWriting.js
+++ coding/codeWriting.js
@@ -818,3 +818,633 @@ Promise.prototype.finally=function(callback){
 }
 
 https://juejin.cn/post/6870319532955828231#heading-42
+
+//实现一个异步求和函数；
+function asyncAdd(a,b,callback){
+   setTimeout(()=>{  
+      callback(null,a+b)
+   },1000);
+}
+// 异步两数之和；
+async function  sumT(a,b){
+   return await new Promise((resolve,reject)=>{
+      asyncAdd(a,b,(err,res)=>{
+         if(!err){
+            resolve(res);
+         }
+         reject(err);
+      })
+   })
+}
+
+// 多数之和呢
+
+const arr=[1,2,3,4]
+const reducer=(acc,cur)=>{
+   return acc+cur
+}
+arr.reduce(reducer);
+
+
+// 利用两数之和 ；
+async function sum(...args){
+   if(args.length===1){
+      return args[0]
+   }
+   let result=[];
+   for(let i=0;i<args.length;i=+2){
+      result.push(sumT(args[0],args[1]));
+   }
+   // 判断是否是奇行；
+   if(args.length&1){
+      result.push(args.length-1):
+   }//
+   return sum(...await Promise.all(result));
+}
+
+// 手写一个Promise.prototype.finally();
+// finally是一个callback回调函数无论前面的返回结果是什么，他都执行；且返回一个promise;
+
+Promise.prototype.finally=function(callback){
+   return this.then((res)=>{
+      return Promise.resolve(callback()).then(()=>{
+        return res; 
+      });
+   },(err)=>{
+      return Promise.resolve(callback()).then(()=>{
+         throw err;
+      });
+   })
+};
+
+// promsie.any
+const promises = [
+  Promise.reject('ERROR A'),
+  Promise.reject('ERROR B'),
+  Promise.resolve('result'),
+]
+
+Promise.any(promises).then((value) => {
+  console.log('value: ', value)
+}).catch((err) => {
+  console.log('err: ', err)
+})
+
+const promises = [
+  Promise.reject('ERROR A'),
+  Promise.reject('ERROR B'),
+  Promise.reject('ERROR C'),
+]
+
+Promise.any(promises).then((value) => {
+  console.log('value：', value)
+}).catch((err) => {
+  console.log('err：', err)
+  console.log(err.message)
+  console.log(err.name)
+  console.log(err.errors)
+})
+
+// 返回一个promise;手写promise.any 遍历promises ，只要一个resove则resolvr;
+Promsie.prototype.any=function(promises){
+   return new Promise((resolve,reject)=>{
+      promises=Array.isArray(promises)?promises:[];
+      let len=promises.length;
+      let err=[];
+      if(len==0){
+         return reject(new AggregateError(''))
+      }
+      // 遍历；
+      promises.forEach((promise)=>{
+         promsie.then((val)=>{
+            resolve(val);
+         },(err)=>{
+            len--;
+            err.push(err);
+            if(len==0){
+               reject(new AggregateError(err));
+            }
+         });
+      });
+   })
+}
+
+
+// coding promsie.allSettled
+
+Promise.prototype.allSettled=function(promises){
+   return new Promise((resolve,reject)=>{
+      let res=[];
+      let times=0;
+      let setData=(index,data)=>{
+         res[index]=data;
+         if(++times===promises.length){
+            resolve(res);
+         }
+      }
+      for(let i=0;i<promises.length;i++){
+         let curr=promises[i]
+         if(isPromsie(curr)){
+            curr.then((res)=>{
+               setData(i,{status:'fullfilled',value:res});
+            },(err)=>{
+               setData(i,{status:'rejected',value:err});
+            }
+         }else{
+            setData(i,{status:'fullfilled',value:curr});
+         }
+      }
+   });
+}
+
+// 监听一个对象；
+
+function observer(obj){
+   Object.keys(obj).forEach(key){
+      defineReactive(obj,key,obj[key])
+   } 
+}
+function defineReactive(obj,k,v){
+   if(typeof v==='object'){
+      observer(v);
+   }
+   Object.defineProperty(obj,k,{
+      enumerable:true,
+      configrable:true,
+      get:function reactiveGetter(){
+         return v;
+      },
+      set:function reactiveSetter(newV){
+         if(v!=newV){
+            v=newV;
+         }
+      }
+   });
+}
+
+const arrayProto=Array.prototype;
+export const arrayMethods=Object.create(arrayProto);
+// 重写以下函数；
+const methodsToPath=['push','pop','shift','unshift','splice',
+'sort','reverse']
+
+methodsToPath.forEach((method)=>{
+   // 缓存原生函数；
+   const original=arrayProto[method]
+   // 重写函数；
+   def(arrayMethods,method,function mutator(...args){
+      // 调用原生函数获取结果；
+      const result =original.apply(this,args)
+      const ob=this.__ob__;
+      let inserted;
+      switch ((method)) {
+         case 'push':
+         case:'unshift':
+            inserted=args
+            break;
+         case:'splice':
+         inserted=args.slice(2)
+            break;
+         default:
+            break;
+      }
+      if(inserted){
+         ob.observerArray(inserted);
+      }
+      // 手动派发更新；
+      ob.dep.notify();
+      return result;
+   })
+});
+
+
+// vm.$set();
+export function set(target,key,val){
+   // target 如果为数组；
+   if(Array.isArray(target)&&isInvalidArrayIndex(key)){
+      target.length=Math.max(target.length,key);
+      target.splice(key,1,val);
+      return val;
+   }//
+  
+   if(key in target && !(key in target.prototype)){
+      target[kye]=val
+      return val;
+   }
+
+   // 判断是否响应式‘
+   const ob=target.__ob__;
+   if(!ob){  // target 不是响应式数据；
+      target[key]=val;
+      return val;
+   }
+
+   // 关键是手动调用，否则进行响应式处理；并通知；ob.dep.notify();
+   defineReactive(ob.value,key,val);
+   ob.dep.notify();
+   return val;
+
+};
+// 手写一个reactive;
+
+const toProxy= new WeekMap():
+const toRaw= new WeekMap();
+function reactive(target){
+   // reactive
+   createReactiveObject(target);
+}
+function createReactiveObject(target){
+   if(!isObject(target)){
+      return target
+   }
+   //
+   let observed=toProxy.get(target);
+   if(observed){
+      return observed
+   }
+   if(toRao.has(target)){
+      return target
+   }
+   const handler={
+      get(target,key,receiver){
+         let res=Reflect.get(target,key,receiver);
+         track(target,'get',key);
+         return res;
+      },
+      set(target,key,val,receiver){
+         let oldV=target[key]
+         let hadkey=target.hasOwnProperty(key);
+         let res=Reflect.set(target,key,val,receiver);
+         if(hadkey){
+            track(target,'set',key);
+         }else{
+            track(target,'addd',key)
+         }
+         return res;
+      }// 
+   }// 
+   let proxy=new Proxy(target,handler);
+   toProxy.set(target,proxy);
+   toRaw.set(proxy,target); 
+};
+
+function rgb2hex(rgb){
+   rgb=rgb.match(/\d+/g);
+   const hex=(n)=>{
+      return ("0"+Number(n).toString(16)).slice(-2);
+   }
+   return rgb.reduce((acc,cur)=>{acc+hex},"#");
+}
+   
+// 插入排序；
+function insertionSort(arr){
+   let n=arr.length;
+   let prevIndex,current;
+   for(let i=1;i<n;i++){
+      current=arr[i];
+      prevIndex=i-1;
+      while(prevIndex>=0&&current<arr[prevIndex]){
+         arr[prevIndex+1]=arr[prevIndex]
+         prevIndex--
+      }
+      curr[prevIndex+1]=current
+   }// end of for ;
+   return arr;
+}
+
+// 闭包js基础题；
+// 1、闭包收集参数 2、getValue进行求和操作；
+var foo = function(...args) { // 要求实现函数体}
+var f1 = foo(1,2,3); f1.getValue(); // 6 输出是参数的和
+var f2 = foo(1)(2,3); f2.getValue(); // 6
+var f3 = foo(1)(2)(3)(4); f3.getValue(); // 10
+
+var foo=function(...args){
+   let addArgs=[...args]
+   function fn(...innerArgs){
+      addArgs=[...innerArgs,...args]
+      return fn;
+   }
+   fn.getValue=function(){
+      return addArgs.reduce((acc,cur)=>{
+         return acc+cur
+      },0);
+   }
+}
+
+var debounce=function (fn,wait=50){
+   let timer,context,args;
+  
+   const later=setTimeout(()=>{
+      fn.apply(context,args);
+      context=null;
+      args=null;
+   },wait);
+    // 使用闭包；
+   return function (...params){
+      
+      if(!timer){
+         timer=later();
+         context=this;
+         args=params;
+      }else{
+         clearTimeout(timer)
+         timer=later();
+
+      }
+   }
+}
+
+// 柯里化curry
+function curry(fn,...args){
+    //将函数的参数综合与当前传入的参数长度进行比较；
+   if(fn.length===args.lenght){
+      return fn(...args)
+   }
+   return function anonymous(...otherArgs){
+      let all=[...args,...otherArgs]
+      return curry(fn,all);
+   }
+}
+// instanceof 内部原理实现；
+function instanceof(L,R){
+   let prototype=R.prototype;
+   let lProto=L.__proto__;
+   while(true){
+      if(L==null){
+         return false;
+      }
+      if(lProto===prototype){
+         return  true;
+      }
+      lProto=lProto.__proto__;
+   }
+}
+
+//按照 instanceof 的逻辑，真正决定类型的是 prototype
+
+function isFunction(value) {
+  return Object.prototype.toString.call(value) === "[object Function]"
+}
+function isDate(value) {
+  return Object.prototype.toString.call(value) === "[object Date]"
+}
+function isRegExp(value) {
+  return Object.prototype.toString.call(value) === "[object RegExp]"
+}
+
+// indexOf / findIndex  //的第一个元素的索引
+Array.prototype.indexOf();
+
+//第 161 题：用最精炼的代码实现数组非零非负最小值index
+例如：[10,21,0,-7,35,7,9,23,18] 输出5, 7最小
+
+function getIndex(arr){
+      let index=null;
+      ...
+      return index;
+}
+
+function getIndex(arr){
+   let index=-1;
+   let min=Infinity;
+   for(let i=0;i<arr.length;i++){
+      if(arr[i]>0&&arr[i]<min){
+         min=arr[i]
+         index=i;
+      }
+   }// end of for;
+   return index;
+}
+// O(N) O(1)
+
+
+// 第 160 题：输出以下代码运行结果，为什么？如果希望每隔 1s 输出一个结果，
+//应该如何改造？注意不可改动 square 方法 #389
+
+const list = [1, 2, 3]
+const square = num => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(num * num)
+    }, 1000)
+  })
+}
+
+function test() {
+  list.forEach(async x=> {
+    const res = await square(x)
+    console.log(res)
+  })
+}
+test()
+// forEach 是否不能阻塞的，默认是请求并行发起，所以同时输出1,4,9；
+
+// 串行输出；
+async function tes(){
+   for(let val of list){
+      const res=await square(val);
+      console.log(res);
+   }
+}
+// promsie版本；
+// 利用promise 本身的链式调用来实现串行；
+let promsie=new Promise();
+function test(i=0){
+   if(i===list.length){
+      return ;
+   }
+   promsie=promsie.then(()=>{
+      square(list[i]);
+   });
+   test(i+1)
+}
+
+// 第 159 题：实现 Promise.retry，成功后 resolve 结果，
+//失败后重试，尝试超过一定次数才真正的 reject #387
+Promise.prototype.retry=function(promsieFn,times=3){
+return new Promsie(async (resolve,reject)=>{
+   while(times--){
+      try{
+         let res=await promsieFn();
+         resolve(res);
+         break;
+      }catch(e){
+         if(!times){
+            reject(e);
+         }
+      }
+   }
+});
+}
+
+// 第 158 题：如何模拟实现 Array.prototype.splice
+let splice=function(start,deleteCount,...addList){
+   if(start<0){
+      if(Math.abs(start)>this.length){
+         start=0;
+      }else{
+         start+=this.length;
+      }
+   }// 获取下标；
+   if(typeof deleteCount==='undefined'){
+      deleteCount=this.length-start;
+   }// 如果不传，则删除数量为start之后的所有；
+
+   // splice 返回移除以后的数组；
+   const removeList=this.slice(start,start+deleteCount);
+
+   const right=this.slice(start+deleteCount);
+   let addIndex=start;
+   addList.concat(right).forEach((item)=>{
+      this[addIndex]=item;
+      addIndex++
+   });
+   this.length=addIndex;
+   // 
+   return removeList
+}
+
+
+
+// 第 156 题：求最终 left、right 的宽度（变形)
+<div class="container">
+    <div class="left"></div>
+    <div class="right"></div>
+</div>
+
+<style>
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  .container {
+    width: 600px;
+    height: 300px;
+    display: flex;
+  }
+  .left {
+    flex: 1 2 300px;
+    background: red;
+  }
+  .right {
+    flex: 2 1 200px;
+    background: blue;
+  }
+</style>
+
+// flex-grow  flex-shrink flex-basic的计算；
+// 多余100；宽度，应该扩大；
+// 括大空间的占比 1/2
+
+//https://developer.mozilla.org/zh-CN/docs/Web/CSS/CSS_Flexible_Box_Layout/Controlling_Ratios_of_Flex_Items_Along_the_Main_Ax
+
+
+// 4. 求最终 left、right 的宽度 #5
+<div class="container">
+    <div class="left"></div>
+    <div class="right"></div>
+</div>
+
+<style>
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  .container {
+    width: 600px;
+    height: 300px;
+    display: flex;
+  }
+  .left {
+    flex: 1 2 500px;
+    background: red;
+  }
+  .right {
+    flex: 2 1 400px;
+    background: blue;
+  }
+</style>
+// 计算flex-shrink  溢出则进行缩小；
+//总权重： 2*500+1*400=1400；
+300*2*500/1400
+300*1*400/1400
+
+
+// 第 153 题：实现一个批量请求函数 multiRequest(urls, maxNum) #378
+
+// 要求如下：
+
+// 要求最大并发数 maxNum
+// 每当有一个请求返回，就留下一个空位，可以增加新的请求
+// 所有请求完成后，结果按照 urls 里面的顺序依次打出
+new Promise((resolve,reject)=>{
+
+});
+
+// 最大并发数请求的实现；
+function multiRequest(urls,maxNum){
+   let result=[];
+   let i=0;
+   let resolve;
+   const promsie=new Promise((res,rej)=>{
+      resolve=res;
+   });
+   const addTask=function(){
+      if(i>=urls.length){
+         resolve();
+      }
+      const task=request(urls[i++]).finally(()=>{
+         addTask()
+      })
+      result.push(task);
+   }
+   // 先执行addTask;
+   while(i<maxNum){
+      addTask();
+   }
+   return promsie.then(()=>{
+      Promise.all(result);
+   });
+}
+
+//第 152 题：实现一个 normalize 函数，能将输入的特定的字符串转化为特定的结构化数据 #331
+字符串仅由小写字母和 [] 组成，且字符串不会包含多余的空格。
+示例一: 'abc' --> {value: 'abc'}
+示例二：'[abc[bcd[def]]]' --> {value: 'abc', children: {value: 'bcd', children: {value: 'def'}}}
+
+// ["abc", "bcd", "def"]
+
+function normalize(s){
+   let arr=s.match(/\w+/g);
+   let result
+   while(arr.length){
+      let cur=arr.pop();
+      let temp={value:cur}
+      if(result){
+         temp.children=result;
+      }
+      result=temp;
+   }// 
+   return result;
+}
+
+// 第 151 题：用最简洁代码实现indexOf方法 #321  slice(start,end);
+
+function indexOf(str,item){
+   let strLen=str.length;
+   let itemLen=item.length;
+   for(let i=0;i<=strLen-itemLen;i++){
+      // 校验是否存在子串；
+      if(str.slice(i,itemLen+i)===item){
+         return i;
+      }
+   }
+   return -1;
+}
+
+https://github.com/Advanced-Frontend/Daily-Interview-Question/issues?page=4&q=is%3Aissue+is%3Aopen
+
+
+
+
